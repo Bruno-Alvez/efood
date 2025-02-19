941c225029895a8bf9d5f2b87f5ead5371afc0e5
Bruno-Alvez <bruno.bsantos75@gmail.com>
2025-02-19 15:02:21 -0300

    finalizando o projeto.

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+import { breakpoints, Colors } from '../../styles'
+
+export const HeaderBar = styled.div`
+  background-color: ${Colors.colorPrimary};
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+  padding: 24px;
+  display: flex;
+  height: 240px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    height: 220px;
+    padding: 16px;
+  }
+`
+
+export const Subtitle = styled.h2`
+  font-size: 32px;
+  font-weight: bold;
+  width: 540px;
+  text-align: center;
+  margin-top: 12px;
+
+  @media (max-width: ${breakpoints.desktop}) {
+    width: 460px;
+    font-size: 30px;
+  }
+
+  @media (max-width: ${breakpoints.tablet}) {
+    width: 300px;
+    font-size: 24px;
+  }
+`
